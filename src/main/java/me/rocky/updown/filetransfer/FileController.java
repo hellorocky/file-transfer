9cb5f8a40959b4d5299d250a1a723b4af0b15935
RockyWu <rocky@rockydeMacBook-Pro.local>
2018-11-10 00:10:31 +0800

    first commit

--- src/main/java/me/rocky/updown/filetransfer/FileController.java
+++ src/main/java/me/rocky/updown/filetransfer/FileController.java
@@ -1,25 +1,37 @@
 package me.rocky.updown.filetransfer;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/")
 public class FileController {
-    @GetMapping
-    public String index(){
+
+    @Value("${file-store-path}")
+    private String fileStorePath;
+
+    @GetMapping("/{filename}")
+    public String index(@PathVariable String filename, HttpServletResponse response){
+        Path path = Paths.get(fileStorePath+filename);
+        FileSystemResource fileSystemResource= new FileSystemResource(path);
+        fileSystemResource.getInputStream();
+        response.
         return "Hello, World!";
     }
 
     @PutMapping("/{filename}")
-    public String upload(@PathVariable String filename,@RequestBody byte[] data){
-        System.out.println(filename);
-        System.out.println(data);
+    public String upload(@PathVariable String filename, @RequestBody byte[] data){
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream("/Users/rocky/tmp/up.jpg");
+            FileOutputStream fileOutputStream = new FileOutputStream(fileStorePath + filename);
             fileOutputStream.write(data);
             fileOutputStream.close();
         } catch (FileNotFoundException e) {
